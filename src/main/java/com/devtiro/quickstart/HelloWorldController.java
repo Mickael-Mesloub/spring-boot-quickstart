33f5ee817c2e7dd9555c25a14ae914b201c612ff
Mickael <mickael.mesloub@gmail.com>
2025-07-18 22:17:11 +0200

    build our first controller to display Hello World on /hello endpoint

--- src/main/java/com/devtiro/quickstart/HelloWorldController.java
+++ src/main/java/com/devtiro/quickstart/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.devtiro.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello Me!";
+    }
 }
--- src/main/java/com/devtiro/quickstart/QuickstartApplication.java
+++ src/main/java/com/devtiro/quickstart/QuickstartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuickstartApplication {
-
+	// entry point of the application
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
