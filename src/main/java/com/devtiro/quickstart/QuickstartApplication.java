8a566f28808ca1bf9c8c57ad69ecd97fa2a62486
Mickael <mickael.mesloub@gmail.com>
2025-07-23 15:48:13 +0200

    refactor(env variables): create printer that prints pizza information using config/PizzaConfig file and env variables defined in application.properties file + lombok for some useful annotations

--- src/main/java/com/devtiro/quickstart/QuickstartApplication.java
+++ src/main/java/com/devtiro/quickstart/QuickstartApplication.java
@@ -1,13 +1,28 @@
 package com.devtiro.quickstart;
 
+import com.devtiro.quickstart.config.PizzaConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuickstartApplication {
+public class QuickstartApplication implements CommandLineRunner {
+	private PizzaConfig pizzaConfig;
+
+	public QuickstartApplication(PizzaConfig pizzaConfig) {
+		this.pizzaConfig = pizzaConfig;
+	}
 	// entry point of the application
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) {
+		System.out.printf("I want a %s crust pizza, with %s and %s sauce%n",
+        pizzaConfig.getCrust(),
+        pizzaConfig.getTopping(),
+        pizzaConfig.getSauce()
+        );
+	}
 }
